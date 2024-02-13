049e921e412afef9af57192765eaf12c56ec6434
Mustapha <mustapha.genouiz@etudiant-fst.utm.tn>
2024-02-13 10:32:18 +0100

    préparation de l'environement des BD

--- mdpGenerator/mdpGenerator/settings.py
+++ mdpGenerator/mdpGenerator/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
