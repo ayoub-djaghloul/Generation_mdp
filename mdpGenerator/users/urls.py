431a7c58d5d29ef9fc80c2ef9e819d0f2e7ab8ab
ayoub-djaghloul <ayoubdj19@gmail.com>
2024-03-04 08:57:13 +0100

    version finale

--- mdpGenerator/users/urls.py
+++ mdpGenerator/users/urls.py
@@ -6,8 +6,6 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('', views.login_signup, name='login_signup'),
-    path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
-    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page=reverse_lazy('login_signup')), name='logout'),
 ]
--- mdpGenerator/users/views.py
+++ mdpGenerator/users/views.py
@@ -44,22 +44,9 @@ def login_signup(request):
 def home(request):
     return render(request, 'users/home.html')
 
-def login(request):
-    return render(request, 'users/login.html')
 @login_required(login_url='/')
 def profile(request):
     user_accounts = compte.objects.filter(user=request.user)
     return render(request, 'users/profile.html', {'user_accounts': user_accounts})
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi {username}, your account has been created!')
-            return redirect('home')
-    else:
-        form = UserRegisterForm()
 
-    return render(request, 'users/signup.html', {'form': form})
